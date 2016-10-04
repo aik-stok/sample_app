6c65acf951d5f12b8eb5c063d8c194a6092e2284
Alexandr Kolesenko <aikb2b@gmail.com>
2016-10-04 14:22:02 +0300

    Make basic user model

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+def setup
+@base_title = "Aik sample_app"
+end
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home"
+    assert_select "title", "Aik sample_app"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-        assert_select "title", "Help"
+        assert_select "title", "Help | Aik sample_app"
 
   end
   test "should get about" do
     get :about
     assert_response :success
-        assert_select "title", "About"
+        assert_select "title", "About | Aik sample_app"
 
   end
 end
