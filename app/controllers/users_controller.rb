b337c9e82a8ba232a2cdd579ea1c185a1866a743
Alexandr Kolesenko <aikb2b@gmail.com>
2016-10-09 18:54:38 +0300

    User signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
   def new
+  @user = User.new
   end
-end
+  def show
+  @user = User.find_by(params[:id])
+  end
+ 
+  def create
+    @user = User.new(user_params)    
+    if @user.save
+    flash[:success] = "Welcome to the Sample App!"
+          redirect_to @user
+      # Handle a successful save.
+    else
+      render 'new'
+      end
+    end
+    private
+  
+    def user_params
+    params.require(:user).permit(:email, :name, :password, :password_confirmation)
+    end
+  end
